c4b8a71b6baa8ccd3e7c84d104f9733b5cd941fc
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2024-01-16 02:01:45 +0300

    color scheme setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,32 @@
-print("hello")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins={
+--start
+{"catppuccin/nvim",name="catppuccin",priority=1000}
+--
+
+
+--end
+}
+local opts={}
+
+require("lazy").setup(plugins,opts)
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
